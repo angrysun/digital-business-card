093dcd468869d324cbe3397364f7a3e2ac10d3d0
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-13 18:27:55 +0900

    About and Interests info added

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,10 @@
 export default function About() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>About</h1>
+      <p>I'm a Tokyo based Fullstack Developer originally from New York City.
+        I enjoy programming because it provides a good test of my problem-solving skills as well as pushing my creativity to new heights.
+      </p>
     </div>
   )
 }
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,7 +1,10 @@
 export default function Interests() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Interests</h1>
+      <p>In my free time I enjoy going for a run or making some gluten-free pizza!
+        I also like traveling, gaming, and relaxing near a river-side.
+      </p>
     </div>
   )
 }
