8a954ced7332d4e9c8f0c937a648c3e187b3e9cf
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-13 19:09:00 +0900

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <p>I'm a Tokyo based Fullstack Developer originally from New York City.
+    <div className="center-body">
+      <h1 className="title-about">About</h1>
+      <p className="title-details">I'm a Tokyo based Fullstack Developer originally from New York City.
         I enjoy programming because it provides a good test of my problem-solving skills as well as pushing my creativity to new heights.
       </p>
     </div>
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,9 +1,9 @@
 export default function Interests() {
   return (
-    <div>
-      <h1>Interests</h1>
-      <p>In my free time I enjoy going for a run or making some gluten-free pizza!
-        I also like traveling, gaming, and relaxing near a river-side.
+    <div className="center-body">
+      <h1 className="title-about">Interests</h1>
+      <p className="title-details">In my free time I enjoy going for a run or making some gluten-free pizza!
+        I also like traveling, gaming, and relaxing near a riverside.
       </p>
     </div>
   )
