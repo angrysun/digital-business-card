5078c594c58c1e4c8d646b4c727c9d93f83b2a4e
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-13 16:10:12 +0900

    info and fontawesome added

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,15 @@
+import profile from '../images/profile.JPG'
+
 export default function Info() {
   return (
     <div>
-      <h1>Hello</h1>
+      <img src={profile} alt="Profile Pic" />
+      <h1>Steven Vasquez</h1>
+      <h2>Fullstack Developer</h2>
+      <h4>www.stevenvasquez.me</h4>
+      <a href="mailto:s.vasquez85@gmail.com?subject=Getting in touch" target="_blank" rel="noreferrer" id="button-gray"><i
+        class="fa-solid fa-square-envelope"></i>Mail</a>
+      <a href="https://www.linkedin.com/in/stevenvasquez85/" rel="noreferrer" target="_blank" id="button-linkedIn"><i class="fa-brands fa-linkedin"></i></a>
     </div>
   )
 }
