b47c5f719352c9b7a88153681c03e5f3c36c1b4c
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-14 11:12:41 +0900

    changed a few words

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
       <h1 className="header">About</h1>
       <p className="about-content">I'm a Tokyo based Fullstack Developer originally from New York City.
         I like using React because its flexible and has great performance because of Virtual DOMs.
-        React's reusable components make it easier to create webpages and sites will have faster load times.
+        React's reusable components make it easier to create webpages with faster load times.
         And being able to write HTML directly in JavaScript code with JSX is very useful.
       </p>
     </div>
