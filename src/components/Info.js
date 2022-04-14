d6a7c4b56fb7d0f5a6cde0445f17c6c454ed5b0e
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-14 10:31:55 +0900

    changed button and page layout a bit

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,11 +2,11 @@ import profile from '../images/profile.JPG'
 
 export default function Info() {
   return (
-    <div>
+    <div className='info'>
       <img src={profile} alt="Profile Pic" className='profile-img'/>
       <h1 className='title-name'>Steven Vasquez</h1>
       <h2 className='title-job'>Fullstack Developer</h2>
-      <a href='https://www.stevenvasquez.me' target="_blank" rel="noreferrer" className='title-sitename'>www.stevenvasquez.me</a>
+      <a href='https://www.stevenvasquez.me' target="_blank" rel="noreferrer" className='title-sitename'><span>www.stevenvasquez.me</span></a>
       <element className="button">
         <a href="mailto:s.vasquez85@gmail.com?subject=Getting in touch" target="_blank" rel="noreferrer" id="button-white"><i
           class="fa-solid --fa-li-margin fa-lg fa-square-envelope"></i>Email</a>
