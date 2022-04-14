b507c33da7eac5175ad7d9b8fe8bb0a2cffe705d
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-14 14:27:57 +0900

    dark mode implemented

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import About from './components/About'
 import Info from './components/Info';
 import Interests from './components/Interests'
 import Footer from './components/Footer'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Toggle from "react-toggle";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
+  const storedDarkMode = localStorage.getItem("dbc-dark-mode");
+  const [darkMode, setDarkMode] = useState(storedDarkMode);
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+
+  useEffect(() => {
+    console.log(`Is in dark mode? ${darkMode}`);
+    localStorage.setItem("dbc-dark-mode", darkMode);
+  }, [darkMode]);
 
   return (
     <div className="App" data-theme={darkMode ? "dark" : "light"}>
       <Info />
+      <Toggle
+        className="dark-mode-toggle"
+        checked={darkMode}
+        onChange={toggleDarkMode}
+        icons={{ checked: "🌙", unchecked: "🔆" }}
+        aria-label="Dark mode toggle"
+      />
+      <button onClick={toggleDarkMode}>
+        {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+      </button>
       <About />
       <Interests />
       <Footer />
--- src/components/Info.js
+++ src/components/Info.js
@@ -7,11 +7,11 @@ export default function Info() {
       <h1 className='title-name'>Steven Vasquez</h1>
       <h2 className='title-job'>Fullstack Developer</h2>
       <a href='https://www.stevenvasquez.me' target="_blank" rel="noreferrer" className='title-sitename'><span>www.stevenvasquez.me</span></a>
-      <element className="button">
+      <div className="button">
         <a href="mailto:s.vasquez85@gmail.com?subject=Getting in touch" target="_blank" rel="noreferrer" id="button-white"><i
           class="fa-solid --fa-li-margin fa-lg fa-square-envelope"></i>Email</a>
         <a href="https://www.linkedin.com/in/stevenvasquez85/" rel="noreferrer" target="_blank" id="button-linkedIn"><i class="fa-brands --fa-li-margin fa-lg fa-linkedin"></i>LinkedIn</a>
-      </element>
+      </div>
     </div>
   )
 }
