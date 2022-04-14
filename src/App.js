ba818b7ee7a0181ba0f9e25a7e0f3f61f580dd10
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-14 19:29:52 +0900

    saved users dark mode choice via localstorage

--- src/App.js
+++ src/App.js
@@ -5,25 +5,34 @@ import Footer from './components/Footer'
 import ToggleButton from './components/ToggleButton';
 import { useState, useEffect } from 'react';
 
-const Checked = () => <>🌙</>;
-const UnChecked = () => <>🔆</>;
-
 function App() {
-  const storedDarkMode = localStorage.getItem("dbc-dark-mode");
+  const storedDarkMode = JSON.parse(localStorage.getItem("dbc-dark-mode"));
   const [darkMode, setDarkMode] = useState(storedDarkMode);
   const toggleDarkMode = () => setDarkMode(!darkMode);
 
   useEffect(() => {
-    console.log(`Is in dark mode? ${darkMode}`);
+    // console.log(`Is in dark mode? ${darkMode}`);
     localStorage.setItem("dbc-dark-mode", darkMode);
   }, [darkMode]);
+
+  // useEffect(() => {
+  //   const prefersDark = window.matchMedia(
+  //     "(prefers-color-scheme: dark)"
+  //   ).matches;
+
+  //   if (prefersDark) {
+  //     darkMode(true);
+  //   }
+  // }, [darkMode]);
 
   return (
     <div className="App" data-theme={darkMode ? "dark" : "light"}>
       <Info />
-      <ToggleButton />
-      <p>Toggle Button - Icons Change</p>
-      <ToggleButton onChange={state => console.log(state)} icons={{ checked: <Checked />, unchecked: <UnChecked /> }} />
+      <ToggleButton
+        onChange={toggleDarkMode}
+        icons={{ checked: '🔆', unchecked: '🌙' }}
+        aria-label="Dark mode"
+      />
       <About />
       <Interests />
       <Footer />
