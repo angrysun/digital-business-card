6ef3e1a34c79317e6aa23d52614d6862b78cc2df
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-15 13:51:47 +0900

    margin change

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div className="center-body">
-      <h1 className="header">About</h1>
+      <h1 className="about-header">About</h1>
       <p className="about-content">I'm a Tokyo based Fullstack Developer originally from New York City.
         I like using React because its flexible and has great performance because of Virtual DOMs.
         React's reusable components make it easier to create webpages with faster load times.
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <a href="https://github.com/angrysun" target="_blank" rel="noreferrer" id="social-icon"><i class="fa-brands fa-github-square"></i></a>
-      <a href="https://twitter.com/explore" target="_blank" rel="noreferrer" id="social-icon"><i class="fa-brands fa-twitter-square"></i></a>
+      <a href="https://twitter.com/angrysunjp" target="_blank" rel="noreferrer" id="social-icon"><i class="fa-brands fa-twitter-square"></i></a>
       <a href="https://www.facebook.com/zuck" target="_blank" rel="noreferrer" id="social-icon"><i class="fa-brands fa-facebook-square"></i></a>
       <a href="https://www.instagram.com/" target="_blank" rel="noreferrer" id="social-icon"><i class="fa-brands fa-instagram-square"></i></a>
     </footer>
